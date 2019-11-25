a617c98e517742c917ea67b656e891ad248ba601
Reed Haffner <reedhaffner@pm.me>
2019-11-24 22:17:42 -0800

    Alert user if there's an error filling the input

--- xkext.js
+++ xkext.js
@@ -4,28 +4,40 @@ browser.menus.create({
     contexts: ["editable", "password"],
     onclick(info, tab) {
 
-        function setCurrentChoice(result) {
+        function getPassword(result) {
             var request = new XMLHttpRequest();
             request.open('GET', `https://xkapi.hfnr.cc/api?separators=-&pattern=${result.preferred_formatting}&transform=random`, false);
             request.send(null);
 
             if (request.status === 200) {
                 browser.tabs.executeScript(tab.id, {
-                    frameId: info.frameId,
-                    code: `browser.menus.getTargetElement(${info.targetElementId}).value = '${request.responseText}';`,
+                    code: `
+                    try {
+                        // Preferred method of inserting the password.
+                        browser.menus.getTargetElement(${info.targetElementId}).value = '${request.responseText}';
+                    } catch (err) {
+                        // Fallback method, which just copies then pastes then resets the clipboard to its previous value.
+                        if(confirm("Something went wrong autofilling the password. Would you like to copy it to your clipboard?")) {
+                            navigator.clipboard.writeText('${request.responseText}');
+                        }
+                    }`,
+                }).then(() => {
+                    navigator.clipboard.readText().then(text => {
+                        if (text != request.responseText) {
+                            browser.notifications.create({
+                                "type": "basic",
+                                "iconUrl": browser.extension.getURL("icons/icon-48.png"),
+                                "title": "XK Password Generated",
+                                "message": `Click here to copy to clipboard.`,
+                            });
+                            browser.notifications.onClicked.addListener((id) => {
+                                browser.tabs.executeScript(tab.id, {
+                                    code: `navigator.clipboard.writeText('${request.responseText}');`,
+                                });
+                            })
+                        }
+                    })
                 });
-                browser.notifications.create({
-                    "type": "basic",
-                    "iconUrl": browser.extension.getURL("icons/icon-48.png"),
-                    "title": "XK Password Generated",
-                    "message": `Click here to copy to clipboard.`,
-                });
-                browser.notifications.onClicked.addListener((id) => {
-                    browser.tabs.executeScript(tab.id, {
-                        frameId: info.frameId,
-                        code: `navigator.clipboard.writeText('${request.responseText}');`,
-                    });
-                })
             }
         }
 
@@ -33,8 +45,8 @@ browser.menus.create({
             console.log(`Error: ${error}`);
         }
 
-        var getting = browser.storage.sync.get("preferred_formatting");
-        getting.then(setCurrentChoice, onError);
+        var getting = browser.storage.sync.get();
+        getting.then(getPassword, onError);
 
     }
 })
