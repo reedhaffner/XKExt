43e66e0e7f86eed8b7dcd915223710ac59c89857
Reed Haffner <reedhaffner@pm.me>
2019-12-03 17:44:20 -0800

    Add XK button onto Password elements

---
+++ page-script.js
@@ -0,0 +1,53 @@
+function getOffset(el) {
+    const rect = el.getBoundingClientRect();
+    return {
+        left: rect.left + window.scrollX,
+        top: rect.top + window.scrollY,
+        right: rect.right + window.scrollX,
+        bottom: rect.bottom + window.scrollY
+    };
+}
+
+function getPassword(result) {
+
+}
+
+function getPwd() {
+    var getting = browser.storage.sync.get();
+    getting.then(e => getPassword(e));
+}
+
+
+function getPwdInputs() {
+    var ary = [];
+    var inputs = document.getElementsByTagName("input");
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].type.toLowerCase() === "password") {
+            ary.push(inputs[i]);
+        }
+    }
+    return ary;
+}
+
+for (var i = 0; i < getPwdInputs().length; i++) {
+    var element = getPwdInputs()[i]
+    var styling = getComputedStyle(element)
+    var positioning = getOffset(element)
+    getPwdInputs()[i].classList.add(`xkext-${i}`)
+    document.body.innerHTML += `<a class='xkext-generate-button' id='xkext-${i}' style='cursor: pointer;position: absolute; top: ${positioning.top}px; left:${positioning.right}px;transform: translateX(-100%);line-height: ${styling.lineHeight};padding: ${styling.paddingTop} ${styling.paddingLeft}; margin-top: ${styling.borderTopWidth};'>XK</a>`;
+}
+
+var xkextButtons = document.getElementsByClassName('xkext-generate-button');
+
+for (var k = 0; k < xkextButtons.length; k++) {
+    xkextButtons[k].addEventListener('click', e => {
+        browser.runtime.sendMessage({
+            element: e.target.id
+        });
+    })
+}
+
+browser.runtime.onMessage.addListener((e) => {
+    document.getElementsByClassName(e.element)[0].value = e.pass;
+    navigator.clipboard.writeText(e.pass);
+});
--- xkext.js
+++ xkext.js
@@ -1,40 +1,54 @@
+async function getRandomPassword() {
+    return new Promise((resolve, reject) => {
+        var prefs = browser.storage.sync.get()
+        prefs.then(settings => {
+            var request = new XMLHttpRequest();
+            request.overrideMimeType("text/html");
+            request.open('GET', `https://xkapi.hfnr.cc/api?separators=${escape(settings.separators)}&pattern=${settings.pattern}&transform=${settings.transform}`, false);
+            request.send(null);
+            if (request.status === 200) {
+                resolve({
+                    password: request.responseText,
+                    shouldCopy: settings.copy
+                })
+            }
+        })
+    })
+}
+
 browser.menus.create({
     id: "xkext",
     title: "Create XKPassword",
     contexts: ["editable", "password"],
     onclick(info, tab) {
-
-        function getPassword(result) {
-            var request = new XMLHttpRequest();
-            request.open('GET', `https://xkapi.hfnr.cc/api?separators=${escape(result.separators)}&pattern=${result.pattern}&transform=${result.transform}`, false);
-            request.send(null);
-
-            if (request.status === 200) {
-                browser.tabs.executeScript(tab.id, {
-                    code: `
+        getRandomPassword().then(pi => {
+            browser.tabs.executeScript(tab.id, {
+                code: `
                     try {
                         // Preferred method of inserting the password.
-                        browser.menus.getTargetElement(${info.targetElementId}).value = '${request.responseText}';
+                        browser.menus.getTargetElement(${info.targetElementId}).value = '${pi.password}';
                     } catch (err) {
                         // Fallback method, which just copies then pastes then resets the clipboard to its previous value.
                         if(confirm("Something went wrong autofilling the password. Would you like to copy it to your clipboard?")) {
-                            navigator.clipboard.writeText('${request.responseText}');
+                            navigator.clipboard.writeText('${pi.password}');
                         }
                     }`,
-                }).then(() => {
-                    if (result.copy) {
-                        navigator.clipboard.writeText(request.responseText);
-                    }
-                });
-            }
-        }
-
-        function onError(error) {
-            console.log(`Error: ${error}`);
-        }
-
-        var getting = browser.storage.sync.get();
-        getting.then(getPassword, onError);
-
+            }).then(() => {
+                if (pi.shouldCopy) {
+                    navigator.clipboard.writeText(pi.password);
+                }
+            });
+        })
     }
 })
+
+browser.runtime.onMessage.addListener(e => {
+    browser.tabs.query({
+        currentWindow: true,
+        active: true
+    }).then(tabs => {
+        getRandomPassword().then(pi => {
+            browser.tabs.sendMessage(tabs[0].id, ({ pass: pi.password, element: e.element }));
+        })
+    });
+});
